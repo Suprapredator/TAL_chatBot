01cbe966ceb4bfe2cea8704666e60545801ccc78
supra <quentin.jardet@u-psud.fr>
2018-03-26 16:01:40 +0200

    ok

--- chatBot.py
+++ chatBot.py
@@ -7,10 +7,11 @@ Created on Mon Mar 26 14:37:08 2018
 
 import argparse
 import random
+import parseur
 
 def discussion_mode_1():
     
-    backchannels = ["hm...","Je vois.","Effectivement.","Je suis d'accord.","ZzZzZzZz.","...ok."]
+    backchannels = ["hm...","I see.","Indeed.","I agree.","ZzZzZzZz.","\"Quand l'appétit va, tout va !\" Obélix 1968"]
     old_backchannels = -1
     new_backchannels = -1
     parole = ""
@@ -26,7 +27,35 @@ def discussion_mode_1():
             print("[ChatBot] "+"Au revoir.")
         old_backchannels = new_backchannels
 
+def discussion_mode_2():
+    
+    health = ["sick","ill","cancer","hurt","cold","diarrhea","disease","illness","sickness","malady"]
+    family = ["dad","mom","father","mother","sister","brother","uncle","aunt","family","grandfather","grandmother"]
+    money = ["dollars","euros","rich","salary","gold","silver","spend","bank","coin","bill"]
+    lazy = ["bed","sleep","holydays","lazy","slackness","work","hard","tired","tiresome"]
+    heathstone = ["card","play","quest","package","deck","minion","hero","power","hearthstone"]
+    
+    health_answer = ["You're feeling good?","Have you been sick recently?"]
+    family_answer = ["How is your family?","Do "]
+    
+    answers = {"family":"Family is important, isn't it?","family":"Do you like your family?"}
+    print(answers["family"])
+    parole = ""
+    
+    while(parole != "Au revoir."):
+        parole = input()
+        
+        if(parole != "Au revoir."):
+            parseur.parsage(parole)
+        else:
+            print("[ChatBot] "+"Au revoir.")
+
+
+# _____________________________________________________________________________
+
 if __name__ == "__main__":
+    
+    print("***CUISTOBOT***\n")    
     
     parser = argparse.ArgumentParser()
     parser.add_argument('mode')
@@ -37,6 +66,9 @@ if __name__ == "__main__":
         discussion_mode_1()
     elif args.mode == "2":
         print("mode n°2 selectionne")
-    else:
+        discussion_mode_2()
+    elif args.mode == "3":
         print("mode n°3 selectionne")
+    else:
+        print("Paramètres invalide !")
     
